9eb80bd4740dd72ed7abf102bc146e353e7f8d1e
CanIBeFii <filipe250502@gmail.com>
2024-02-26 22:02:06 +0000

    feat: change Dockerfiles

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-SETUP='/init.sql'
+# Set values on config files with environment variables
+sed -i 's|MARIADB_DATABASE|'${MARIADB_DATABASE}'|g' /tmp/config.sql
+sed -i 's|MARIADB_USER|'${MARIADB_USER}'|g' /tmp/config.sql
+sed -i 's|MARIADB_USER_PASS|'${MARIADB_USER_PASS}'|g' /tmp/config.sql
+sed -i 's|MARIADB_ROOT_PASS|'${MARIADB_ROOT_PASS}'|g' /tmp/config.sql
+sed -i 's|MARIADB_PORT|'${MARIADB_PORT}'|g' /etc/mysql/my_config.cnf
+sed -i 's|MARIADB_ADDRESS|'${MARIADB_ADDRESS}'|g' /etc/mysql/my_config.cnf
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+if [ ! -d "/var/lib/mysql/$MARIADB_DATABASE" ]; then
 	echo "Install mariadb for the first time"
-	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+	mysql_install_db
+	mysqld --init-file="/tmp/config.sql"
+else
+	echo "Database already exists"
+	mysqld_safe
 fi
 
-echo "Setup initial file"
-cat tmpl.sql | envsubst > ${SETUP}
-
-echo "Start mysql daemon to receive arguments"
-exec mysqld --user=mysql --datadir="/var/lib/mysql" --port=3306 --init-file ${SETUP} $@
+exec "$@"
---
+++ srcs/requirements/nginx/tools/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir -p $WORDPRESS_PATH
+chown -R www-data $WORDPRESS_PATH
+
+openssl req -x509 -nodes -days 365 -subj "C=PT/ST=Lisbon/L=Lisbon/O=42Lisbon/OU=42Lisbon/CN=$DOMAIN" \
+	-newkey rsa:2048 -keyout $PRIVATE_KEY -out $CERTIFICATE
+
+sed -i 's|NGINX_PORT|'${NGINX_PORT}'|g' /etc/nginx/sites-available/default.conf
+sed -i 's|DOMAIN|'${DOMAIN}'|g' /etc/nginx/sites-available/default.conf
+sed -i 's|WORDPRESS_PATH|'${WORDPRESS_PATH}'|g' /etc/nginx/sites-available/default.conf
+sed -i 's|PHP_HOST|'${PHP_HOST}'|g' /etc/nginx/sites-available/default.conf
+sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/nginx/sites-available/default.conf
+sed -i 's|PRIVATE_KEY|'${PRIVATE_KEY}'|g' /etc/nginx/sites-available/default.conf
+sed -i 's|CERTIFICATE|'${CERTIFICATE}'|g' /etc/nginx/sites-available/default.conf
+
+nginx -g "daemon off;"
