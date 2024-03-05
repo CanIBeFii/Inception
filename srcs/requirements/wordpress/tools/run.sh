765e09e1a423b5168b48cfbe4fab9500dbd45a0c
CanIBeFii <filipe250502@gmail.com>
2024-03-05 13:26:14 +0000

    feat: change multiple files

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,45 +1,18 @@
 #!/bin/sh
 
-set -x
+sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/php/8.3/fpm/pool.d/www.conf
 
-if [ -e /etc/php7/php-fpm.d/www.conf ]; then 
-	echo "FastCGI Process Manager config already created"
+if [ ! -f "/var/www/html/wordpress/wp-config.php" ] then
+	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+	wp core download --path=$WORDPRESS_PATH --allow-root
+	wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_USER_PASS --dbhost=mariadb --path=$WORDPRESS_PATH --skip-check --allow-root
+	wp core install --path=$WORDPRESS_PATH --url=$DOMAIN --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_USER --admin_password=$WORDPRESS_PASS --admin_email=$WORDPRESS_EMAIL --skip-email --allow-root
+	wp theme install teluro --path=$WORDPRESS_PATH --activate --allow-root
+	wp user create fii fii@fi.in --role=author --path=$WORDPRESS_PATH --user_pass=fii --allow-root
 else
-	cat /ww.conf.tmpl | envsubst > /etc/php7/php-fpm.d/www.conf
+	echo "Wordpress already installed"
 fi
 
-if [ -e wp-config.php ]; then
-	echo "Wordpress config already created"
-else
-	wp config create --allow-root \
-		--dbname=$WORDPRESS_DB_NAME \
-		--dbuser=$MARIADB_USER_NAME \
-		--dbpass=$MARIADB_USER_PASS \
-		--dbhost=$MARIADB_HOST
-
-	wp db create --allow-root
-fi
-
-if [ wp core is-installed --allow-root ]; then 
-	echo "Wordpress core already installed"
-else
-	wp core install --allow-root \
-		--url=$WORDPRESS_URL \
-		--title=$WORDPRESS_TITLE \
-		--admin_user=$WORDPRESS_ADMIN_NAME \
-		--admin_email=$WORDPRESS_ADMIN_EMAIL \
-		--admin_pass=$WORDPRESS_ADMIN_PASS
-
-	wp user create --allow-root \
-		$WORDPRESS_USER_NAME \
-		$WORDPRESS_USER_EMAIL \
-		--role=author \
-		--user_pass=$WORDPRESS_USER_PASS
-
-	wp config set WORDPRESS_DEBUG false --allow-root
-fi
-
-wp plugin update --all --allow-root
-
-echo "Start FastCGI Process Manager"
-exec php-fpm7 --nodaemonize $@
+/usr/sbin/php-fpm8.3 -F
